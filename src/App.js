8b96c114a501eb537add35467b3b38d29e66e1a5
sonjix <sojino0813@naver.com>
2023-04-06 19:41:54 +0900

    new file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import object1 from "../src/images/buildingex.glb";
+import object2 from "../src/images/table1.glb";
+import object3 from "../src/images/table2.glb";
 
-function App() {
+const App = () => {
+  const gltf1 = useLoader(GLTFLoader, object1);
+  const gltf2 = useLoader(GLTFLoader, object2);
+  const gltf3 = useLoader(GLTFLoader, object3);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+      style={{ width: "800px", height: "800px", position: `relative` }}
+      camera={{ position: [13, 13, 13] }}
+      shadows>
+      <primitive
+        object={gltf1.scene}
+        scale={[1, 1, 1]}
+        position={[0, 0, 0]}
+        children-0-castShadow
+      />
+      <primitive
+        object={gltf2.scene}
+        scale={[1, 1, 1]}
+        position={[-2, 0, 0]}
+        children-0-castShadow
+      />
+      <primitive
+        object={gltf3.scene}
+        scale={[1, 1, 1]}
+        position={[2, 0, 0]}
+        children-0-castShadow
+      />
+
+      <directionalLight intensity={1} />
+      <ambientLight intensity={1.2} />
+      <spotLight intensity={0.1} angle={0.1} penumbra={1} castShadow />
+      <OrbitControls target={[0, 1, 0]} />
+    </Canvas>
   );
-}
+};
 
 export default App;
