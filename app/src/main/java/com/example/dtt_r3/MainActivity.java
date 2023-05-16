0a2cfb6ab1360b60b4d0ed94456c4ccd1fcf9d42
zoodung <128677933+zoodung@users.noreply.github.com>
2023-05-16 20:12:56 +0900

    PROJECT INIT

--- app/src/main/java/com/example/dtt_r3/MainActivity.java
+++ app/src/main/java/com/example/dtt_r3/MainActivity.java
@@ -1,64 +1,71 @@
 package com.example.dtt_r3;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE_ORDER1 = 1;
+    private static final int REQUEST_CODE_ORDER2 = 2;
+    private Button table1, table2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button table1 = findViewById(R.id.btn_table1);
-        Button table2 = findViewById(R.id.btn_table2);
+        table1 = findViewById(R.id.btn_table1);
+        table2 = findViewById(R.id.btn_table2);
 
         table1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Order1.class );
-                startActivity(intent);
-                finish();
+                Intent intent = new Intent(MainActivity.this, Order1.class);
+                startActivityForResult(intent, REQUEST_CODE_ORDER1);
             }
         });
 
         table2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Order2.class );
-                startActivity(intent);
-                finish();
+                Intent intent = new Intent(MainActivity.this, Order2.class);
+                startActivityForResult(intent, REQUEST_CODE_ORDER2);
             }
         });
 
-        Intent intent1 = getIntent();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        //DB에서 테이블 상태 정보 갱신
-
-
-        //DB에서 받아온 데이터에 따라 테이블  상태 변경
-
-
-        int confirmValue1 = intent1.getIntExtra("confirm1", 0);
-        if (confirmValue1==1){
-            table1.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_red_light)));
+        if (resultCode == RESULT_OK) {
+            int state = data.getIntExtra("state", 0);
+            int total = data.getIntExtra("total", 0);
+            switch (requestCode) {
+                case REQUEST_CODE_ORDER1:
+                    setTable1ButtonColor(state, total);
+                    break;
+                case REQUEST_CODE_ORDER2:
+                    setTable2ButtonColor(state, total);
+                    break;
+            }
         }
-        int confirmValue2 = intent1.getIntExtra("confirm2", 0);
-        if (confirmValue2==1){
-            table2.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_red_light)));
-        }
-        int payValue1 = intent1.getIntExtra("pay1", 0);
-        if (payValue1==1){
-            table1.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_green_light)));
-        }
-        int payValue2 = intent1.getIntExtra("pay2", 0);
-        if (payValue2==1){
-            table2.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_green_light)));
-        }
+    }
 
+    public void setTable1ButtonColor(int state, int total) {
+        if(state == 1){ table1.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_red_light))); }
+        else if(state == 2){ table1.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_green_light))); }
+        table1.setText("TABLE 1\n￦"+total);
+    }
+
+    public void setTable2ButtonColor(int state, int total) {
+        if(state == 1){ table2.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_red_light))); }
+        else if(state == 2){ table2.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(android.R.color.holo_green_light))); }
+        table2.setText("TABLE 2\n￦"+total);
     }
 }
